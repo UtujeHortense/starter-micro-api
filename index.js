7bab4387cee1640b6af7ead7ab6edae450315479
Hortense <utuje.hortense@epitech.eu>
2023-06-14 18:03:50 +0200

    [FIX] Handle cors error

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var http = require('http');
 http.createServer(function (req, res) {
     if (req.method === 'GET') {
       // Handle the GET request here
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     const options = {method: 'GET',headers: {Accept: 'application/json', 'Content-Type': 'application/json'}};
     fetch('https://xkcd.com/info.0.json', options)
     .then(response => {
